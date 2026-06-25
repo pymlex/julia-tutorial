b03f6f2d7cf53b370eae2a6caa7060802cdcea4d
Alex Zyukov <mhk_apel@outlook.com>
2026-06-25 07:37:29 +0300

    "[17] Sparse matrices.jl"

---
+++ [17] Sparse matrices.jl
@@ -0,0 +1,22 @@
+using LinearAlgebra
+using SparseArrays
+using Printf
+
+n = 6
+dl = fill(-1.0, n - 1)
+d = fill(2.0, n)
+du = fill(-1.0, n - 1)
+
+T = Tridiagonal(dl, d, du)
+println("Tridiagonal T =")
+println(T)
+
+b = ones(n)
+x = T \ b
+println("x = ", x)
+println("residual norm = ", norm(T * x - b))
+
+S = spdiagm(-1 => dl, 0 => d, 1 => du)
+println("\nSparse matrix nnz = ", nnz(S))
+println("Matrix type = ", typeof(S))
+println("Dense and sparse agree = ", norm(Matrix(S) * x - b))
